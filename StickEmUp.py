139cf1f23d6525187c034f2da15f05e94ed1cfe9
Walt <walt@waltnixon.com>
2014-02-26 09:49:55 -0600

    Loading Files

---
+++ StickEmUp.py
@@ -0,0 +1,26 @@
+##StickEmUp
+##Walt Nixon
+##20140226
+
+import StickEmUpFunctions as sf
+import arcpy
+
+#Loop through units 
+#Read Unit Geometery
+
+    #Find Two longest edges 
+    ##Average edges and divide by 2 to get lateral length
+    ##Subtract 50' from lateral length
+
+    #Find unit azimuth 
+
+    #Find unit centoid 
+
+    #From Centroid Calculate beginning and end points for laterals 
+
+    #Store Line point pairs in a list of tuples
+
+#Draw lines
+
+#Clean up lines
+##Run intersect 
---
+++ StickEmUpFunctions.py
@@ -0,0 +1,14 @@
+##StickEmUp Functions
+##Walt Nixon
+##20140226
+
+
+
+#Get Distance between two points function
+#Find two longest edges
+
+#Get angle of a line function
+
+#Draw Lines Function
+
+
